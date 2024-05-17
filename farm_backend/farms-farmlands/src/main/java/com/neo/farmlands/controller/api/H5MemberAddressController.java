4d202aa94c5fb5488d694c0b781743e79647e2c5
neo <y2k00001@126.com>
2024-05-18 00:05:32 +0800

    dev: 1.完善H5用户地址接口

--- farm_backend/farms-farmlands/src/main/java/com/neo/farmlands/controller/api/H5MemberAddressController.java
+++ farm_backend/farms-farmlands/src/main/java/com/neo/farmlands/controller/api/H5MemberAddressController.java
@@ -4,6 +4,8 @@ package com.neo.farmlands.controller.api;
 import com.neo.farmlands.domain.vo.MemberAddressForm;
 import com.neo.farmlands.domain.vo.MemberAddressVO;
 import com.neo.farmlands.service.impl.H5MemberAddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,27 +14,32 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/h5/member/address")
+@Api("用户地址相关接口")
 public class H5MemberAddressController {
 
   @Autowired
   private H5MemberAddressService h5MemberAddressService;
 
   @GetMapping("/list")
+  @ApiOperation("查询用户地址列表")
   public ResponseEntity<List<MemberAddressVO>> getList(){
     return ResponseEntity.ok(h5MemberAddressService.selectList());
   }
 
   @GetMapping("/default")
+  @ApiOperation("查询用户默认地址")
   public ResponseEntity<MemberAddressVO> getDefault(){
     return ResponseEntity.ok(h5MemberAddressService.getDefault());
   }
 
   @PostMapping("/create")
+  @ApiOperation("新增用户地址")
   public ResponseEntity<Integer> create(@RequestBody MemberAddressForm memberAddressForm){
     return ResponseEntity.ok(h5MemberAddressService.insert(memberAddressForm));
   }
 
   @PutMapping("/update")
+  @ApiOperation("更新用户地址")
   public ResponseEntity<Integer> update(@RequestBody MemberAddressForm memberAddressForm){
     return ResponseEntity.ok(h5MemberAddressService.update(memberAddressForm));
   }
