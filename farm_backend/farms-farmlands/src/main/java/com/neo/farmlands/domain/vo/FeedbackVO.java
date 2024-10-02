81e0bf8f967823e9d88fb7575562584e97d78462
neo <y2k00001@126.com>
2024-10-02 23:43:19 +0800

    test: 1.完善注释，注解

--- farm_backend/farms-farmlands/src/main/java/com/neo/farmlands/domain/vo/FeedbackVO.java
+++ farm_backend/farms-farmlands/src/main/java/com/neo/farmlands/domain/vo/FeedbackVO.java
@@ -3,6 +3,7 @@ package com.neo.farmlands.domain.vo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import com.neo.common.annotation.Excel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -16,29 +17,29 @@ public class FeedbackVO  {
    /** ID */
     private Long id;
    /** 类型 */
-    @Excel(name = "类型")
+    @ApiModelProperty(value = "类型")
     private String type;
    /** 具体说明 */
-    @Excel(name = "具体说明")
+   @ApiModelProperty(value = "具体说明")
     private String content;
    /** 图片 */
-    @Excel(name = "图片")
+    @ApiModelProperty(value = "图片")
     private String images;
    /** 联系电话 */
-    @Excel(name = "联系电话")
+    @ApiModelProperty(value = "联系电话")
     private String phone;
    /** 创建人 */
-    private Long createBy;
+    @ApiModelProperty(value = "创建人")
    /** 创建时间 */
     private LocalDateTime createTime;
    /** 处理状态 0：未处理  1：已处理 */
-    @Excel(name = "处理状态 0：未处理  1：已处理")
+    @ApiModelProperty(value = "处理状态 0：未处理  1：已处理")
     private Integer handleStatus;
    /** 备注 */
-    @Excel(name = "备注")
+    @ApiModelProperty(value = "备注")
     private String remark;
    /** 处理时间 */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Excel(name = "处理时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "处理时间")
     private LocalDateTime handleTime;
 }
